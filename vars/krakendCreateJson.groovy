483323d477c48d5c33512e11b4786f6b0fdbd8be
KerimAksak <abdulkerim.aksak@outlook.com>
2024-03-16 16:54:58 +0300

    update krakendCreateJson

--- vars/krakendCreateJson.groovy
+++ vars/krakendCreateJson.groovy
@@ -1,8 +1,8 @@
 def call(Map config=[:]) {
   sh "FC_ENABLE=1 \
-    FC_SETTINGS='$PWD/settings' \
-    FC_PARTIALS='$PWD/partials' \
-    FC_TEMPLATES='$PWD/templates' \
+    FC_SETTINGS='${config.workingDir}/settings' \
+    FC_PARTIALS='${config.workingDir}/partials' \
+    FC_TEMPLATES='${config.workingDir}/templates' \
     FC_OUT=krakend.json \
-    krakend check -t -d -c '$PWD/${config.flexibleFileName}'"
+    krakend check -t -d -c './${config.flexibleFileName}'"
 }
